987e0cbc9e1e07deffb11fe6bca27aae956b0d29
Cassiopeia316 <dvinagunawan@gmail.com>
2022-03-17 17:28:58 +0700

    ejs + partial + bootstrap

--- chapter3/devinagunawan/devinagunawan_project/routes/pages.js
+++ chapter3/devinagunawan/devinagunawan_project/routes/pages.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const page =
  require("../controllers/pagesControllers");
+ require("../controllers/pages.js");
 
  router.get("/", page.home);
+ module.exports = router;
+
+ router.get("/about", page.about);
  module.exports = router;
