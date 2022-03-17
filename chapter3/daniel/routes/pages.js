dd8a090b9b582cb271adfba1cc1c6677b53348e7
Juliant Daniel Latuary <daniel.latuary@gmail.com>
2022-03-17 20:44:17 +0700

    first commit

---
+++ chapter3/daniel/routes/pages.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const page = require("../controllers/pagesController");
+
+router.get("/", page.home);
+router.get("/about", page.about);
+
+module.exports = router;
